cbfe47656104534b8198a57ec69224372ee3652c
dangerousjatt <73658726+dangerousjatt@users.noreply.github.com>
2020-11-12 08:34:45 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,17 +29,11 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of [PAWANBIR 🇮🇳](t.me/I_AM_PAWANBIR)
 
-Special thanks to [this person](t.me/denver02) for helping me.
+HATERS KE MAA KE CHUT MA MERA LUN WITHOUT OIL
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
-
-Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/JATTGROUPMANAGINGBOT?startgroup=true).
 """
 
 
